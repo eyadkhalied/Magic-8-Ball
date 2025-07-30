e8db75d0769464aaac0b6083c73a430581675e7b
eyadkhalied <eyadkhalied2012@gmail.com>
2025-07-30 08:54:15 -0700

    adding

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 
 def get_user_guess():
-    pass
+    while True:
+        try:
+            guess = int(input("Enter your guess (1-100): "))
+            if 1 <= guess <= 100:
+                return guess
+            else:
+                print("Please enter a number between 1 and 100.")
+        except ValueError:
+            print("Invalid input! Please enter a number.")
 
 responses=[]
 def get_random_responses():
